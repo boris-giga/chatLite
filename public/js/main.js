69a78dd6cb0181c087009c0f22f6cbbcd13b323d
boris-giga <boris.ko.uni@gmail.com>
2021-08-11 01:16:18 +0300

    merged 2 htmls, changed logic to work with session storage

--- public/js/main.js
+++ public/js/main.js
@@ -3,44 +3,80 @@ const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name')
 const usersList = document.getElementById('users')
 
-// get username and room from URL
-const urlParams = new URLSearchParams(location.search)
-const [username, room] = [...urlParams.values()]
+const joinRoomBtn = document.querySelector('button.btn');
+const leaveRoomBtn = document.querySelector('a.btn');
 
 const socket = io();
 
-// join chatroom
-socket.emit('joinRoom', {username, room})
+joinRoomBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const chat = document.querySelector('.chat-container')
+  const joinForm = document.querySelector('.join-container')
+  const usernameValue = document.getElementById('username').value
+  const roomValue = document.getElementById('room').value
+  sessionStorage.setItem('username', usernameValue)
+  sessionStorage.setItem('room', roomValue)
+  chat.classList.remove('blr')
+  joinForm.classList.add('dissolve')
+  console.log(`${usernameValue} / ${roomValue} // ${sessionStorage}`)
 
-// get room and users
-socket.on('roomUsers', ({room, users}) => {
-  outputRoomName(room),
-  outputUsers(users)
+// get username and room from URL
+// const urlParams = new URLSearchParams(location.search) // TODO: cut off, add session storage
+// const [username, room] = [...urlParams.values()]
+
+  // get username and room from session storage
+  const {username, room} = sessionStorage
+
+  
+
+  // join chatroom
+  socket.emit('joinRoom', {username, room}) // TODO get from session storage
+
+  // get room and users
+  socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room),
+    outputUsers(users)
+  })
+
+  // message from server
+  socket.on('message', (message) => {
+    console.log(message);
+    outputMessage(message);
+
+    // scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  });
+
+  // message submit
+  chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get message text
+    const msg = e.target.elements.msg.value;
+    
+    // emit message to server
+    socket.emit('chatMessage', msg);
+
+    // clear input and focus
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+  });
 })
 
-// message from server
-socket.on('message', (message) => {
-  console.log(message);
-  outputMessage(message);
+function clearChatMessages() {
+  const chat = document.querySelector('.chat-messages')
+  chat.innerText = ''
+}
 
-  // scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-// message submit
-chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // get message text
-  const msg = e.target.elements.msg.value;
-  
-  // emit message to server
-  socket.emit('chatMessage', msg);
-
-  // clear input and focus
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
-});
+leaveRoomBtn.addEventListener('click', (e) => {
+  clearChatMessages()
+  sessionStorage.clear()
+  const chat = document.querySelector('.chat-container')
+  const joinForm = document.querySelector('.join-container')
+  joinForm.classList.remove('dissolve')
+  chat.classList.add('blr')
+  window.location.reload()
+})
 
 // output message to DOM
 function outputMessage(message) {
@@ -68,4 +104,26 @@ function outputUsers(users) {
   usersList.innerHTML = `
   ${users.map(user => `<li>${user.username}</li>`).join('')}
   `
+}
+
+
+// removes blur from chat
+// function clickJoin() {
+//   const chat = document.querySelector('.chat-container')
+//   const joinForm = document.querySelector('.join-container')
+//   const usernameValue = document.getElementById('username').value
+//   const roomValue = document.getElementById('room').value
+//   sessionStorage.setItem('username', usernameValue)
+//   sessionStorage.setItem('room', roomValue)
+//   chat.classList.remove('blr')
+//   joinForm.classList.add('dissolve')
+//   console.log(`${usernameValue} / ${roomValue} // ${sessionStorage}`)
+// }
+
+function clickLeave() {
+  sessionStorage.clear()
+  const chat = document.querySelector('.chat-container')
+  const joinForm = document.querySelector('.join-container')
+  joinForm.classList.remove('dissolve')
+  chat.classList.add('blr')
 }
